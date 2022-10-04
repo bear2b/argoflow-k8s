5489d7fc1ac3188c916bc2cbd94a8c864361864c
BigBreadman <61837880+BigBreadman@users.noreply.github.com>
2022-10-04 17:59:25 +0200

    BAPI-1718: Update ClickHouse scripts as well as other HELM templates

--- initial/clickhouse/initial.sql
+++ initial/clickhouse/initial.sql
@@ -8,24 +8,27 @@ drop table tracks;
 CREATE DATABASE IF NOT EXISTS wizeflow;
 use wizeflow;
 
+
 CREATE TABLE wizeflow.tracks (
     dt DateTime,
     ms UInt32,
     timezone Nullable(Int16),
-    session_id Nullable(String), 
-    document_id Nullable(String), 
-    document_uuid Nullable(String),
-    content_id Nullable(String),
+    session_id Nullable(String),
+    smartlink_id Nullable(String),
+    document_id Nullable(String),
     organization_id Nullable(String),
+    user_id Nullable(String),
+    batch_id Nullable(String),
     project_id Nullable(String),
-    member_id Nullable(String),
-    user Nullable(String),
+    project_type Nullable(String),
+    visitor Nullable(String),
     email Nullable(String),
     fp Nullable(String),
-    action Nullable(String),
-    duration Nullable(UInt32),
+    object Nullable(String),
+    type Nullable(String),
+    event Nullable(String),
+    data Nullable(String),
     page Nullable(UInt32),
-    page_id Nullable(UInt32),
     asset_id Nullable(String),
     browser_major Nullable(String),
     browser_name Nullable(String),
@@ -50,6 +53,7 @@ PARTITION BY toDate(dt)
 ORDER BY (dt, ms) 
 SETTINGS index_granularity = 8192;
 
+
 CREATE TABLE __kafka__wizeflow_tracks AS tracks ENGINE = Kafka()
 SETTINGS 
 kafka_broker_list='kafka-service:19092',
@@ -63,37 +67,149 @@ CREATE MATERIALIZED VIEW __kafka__wizeflow_tracks__mv TO tracks AS SELECT * FROM
 -- To avoid problem: Too many partitions for single INSERT block (more than 100).
 -- SET max_partitions_per_insert_block=1000
 
-SET max_partitions_per_insert_block=1000;
-CREATE MATERIALIZED VIEW wizeflow.tracks_page_views_by_document
-ENGINE = AggregatingMergeTree() 
-PARTITION BY left(ifNull(document_uuid,'00'),2) 
-ORDER BY ifNull(document_uuid,'0000')
-POPULATE
-AS 
-select ifNull(document_uuid,'0000') document_uuid, countIfState(action='ENTER') views, 
-sumState(duration) view_seconds, 
-uniqExactIfState(fp,action='PAGE_VIEW') devices
-from wizeflow.tracks
-where fp!='' and user!='anonymous'
-group by document_uuid;
+
+CREATE TABLE wizeflow.actions (
+    dt DateTime,
+    ms UInt32,
+    rid String,
+    ua Nullable(String),
+    url Nullable(String),
+    method Nullable(String),
+    entity Nullable(String),
+    entity_id Nullable(String),
+    user_id Nullable(String),
+    user_name Nullable(String),
+    organization_id Nullable(String)
+)
+ENGINE = MergeTree() 
+PARTITION BY toDate(dt)
+ORDER BY (dt, ms, rid) 
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE __kafka__wizeflow_actions AS actions ENGINE = Kafka()
+SETTINGS 
+kafka_broker_list='kafka1:19092',
+kafka_topic_list = 'wizeflow.actions',
+kafka_group_name = 'ch.wizeflow.actions',
+kafka_format = 'JSONEachRow',
+kafka_skip_broken_messages = 1;
+
+CREATE MATERIALIZED VIEW __kafka__wizeflow_actions__mv TO actions AS SELECT * FROM __kafka__wizeflow_actions;
+
+
+CREATE TABLE wizeflow.errors (
+    dt DateTime,
+    ms UInt32,
+    rid String,
+    name Nullable(String),
+    code Nullable(String),
+    message Nullable(String),
+    stack Nullable(String),
+    parameters Nullable(String)
+)
+ENGINE = MergeTree() 
+PARTITION BY toDate(dt)
+ORDER BY (dt, ms, rid) 
+SETTINGS index_granularity = 8192;
+
+CREATE TABLE __kafka__wizeflow_errors AS errors ENGINE = Kafka()
+SETTINGS 
+kafka_broker_list='kafka1:19092',
+kafka_topic_list = 'wizeflow.errors',
+kafka_group_name = 'ch.wizeflow.errors',
+kafka_format = 'JSONEachRow',
+kafka_skip_broken_messages = 1;
+
+CREATE MATERIALIZED VIEW __kafka__wizeflow_errors__mv TO errors AS SELECT * FROM __kafka__wizeflow_errors;
 
 
 SET max_partitions_per_insert_block=1000;
-CREATE MATERIALIZED VIEW wizeflow.tracks_page_views_by_date
+CREATE MATERIALIZED VIEW wizeflow.tracks_views_by_smartlink_and_user_data
 ENGINE = AggregatingMergeTree() 
-PARTITION BY left(ifNull(document_uuid,'00'),2) 
-ORDER BY (document_uuid,date,user,page)
+PARTITION BY left(ifNull(smartlink_id,'00'),2) 
+ORDER BY (ifNull(smartlink_id,'0000'), ifNull(visitor,''), ifNull(email,''), ifNull(fp,''))
 POPULATE
 AS 
-select ifNull(document_uuid,'0000') document_uuid, 
-toDate(dt) date, 
-ifNull(user,'') user,
-ifNull(page,0) page, 
-countIfState(action='OPEN') views,
-maxIfState(dt,action='PAGE_VIEW') last_viewed, 
-sumState(duration) view_seconds, 
-uniqExactIfState(fp,action='PAGE_VIEW') devices
+select ifNull(smartlink_id,'0000') smartlink_id,
+countState() views,
+ifNull(visitor,'') visitor,
+ifNull(email,'') email,
+ifNull(fp,'') fp
 from wizeflow.tracks
-where action in ('PAGE_VIEW','OPEN') and page>0 and fp!='' and user!='anonymous'
-group by document_uuid,toDate(dt),user,page
-order by page;
+where fp!='' and visitor!='anonymous' and object='sl' and event='open'
+group by smartlink_id, visitor, email, fp;
+
+
+SET max_partitions_per_insert_block=1000;
+CREATE MATERIALIZED VIEW wizeflow.tracks_view_by_organization_id_dt
+ENGINE = AggregatingMergeTree() 
+PARTITION BY left(ifNull(organization_id,'00'),2) 
+ORDER BY (organization_id, date, visitor)
+POPULATE
+AS 
+select ifNull(organization_id,'0000') organization_id,
+toDate(dt) date,
+ifNull(visitor,'') visitor,
+countIfState(object='sl' and event='open') views,
+sumIfState(toInt32OrZero(data), object='page' and event='view') view_seconds,
+uniqExactIfState(fp, object='sl' and event='open') devices
+from wizeflow.tracks
+where fp!='' and visitor!='anonymous' and ((object='page' and event='view') or (object='sl' and event='open'))
+group by organization_id, toDate(dt), visitor;
+
+
+SET max_partitions_per_insert_block=1000;
+CREATE MATERIALIZED VIEW wizeflow.tracks_view_by_organization_id_smarlink_id_dt
+ENGINE = AggregatingMergeTree() 
+PARTITION BY left(ifNull(organization_id,'00'),2) 
+ORDER BY (organization_id, user_id, smartlink_id, date, visitor)
+POPULATE
+AS 
+select 
+ifNull(organization_id,'0000') organization_id,
+ifNull(user_id,'0000') user_id,
+ifNull(smartlink_id,'0000') smartlink_id,
+toDate(dt) date,
+ifNull(visitor,'') visitor,
+countState() views
+from wizeflow.tracks
+where fp!='' and visitor!='anonymous' and object='sl' and event='open'
+group by organization_id, user_id, smartlink_id, toDate(dt), visitor;
+
+
+SET max_partitions_per_insert_block=1000;
+CREATE MATERIALIZED VIEW wizeflow.tracks_view_by_smartlink_id_dt
+ENGINE = AggregatingMergeTree() 
+PARTITION BY left(ifNull(smartlink_id,'00'),2) 
+ORDER BY (smartlink_id, date, visitor)
+POPULATE
+AS 
+select ifNull(smartlink_id,'0000') smartlink_id,
+toDate(dt) date,
+countIfState(object='sl' and event='open') views,
+sumIfState(toInt32OrZero(data), object='page' and event='view') view_seconds,
+countIfState(object='sl' and event='open' and os_name IN ('android', 'ios', 'Android', 'iOS')) mobile_view,
+countIfState(object='sl' and event='open' and os_name not IN ('android', 'ios', 'Android', 'iOS')) desktop_view,
+uniqExactIfState(fp, object='sl' and event='open') devices,
+ifNull(visitor,'') visitor
+from wizeflow.tracks
+where fp!='' and visitor!='anonymous' and ((object='page' and event='view') or (object='sl' and event='open'))
+group by smartlink_id, toDate(dt), visitor;
+
+
+SET max_partitions_per_insert_block=1000;
+CREATE MATERIALIZED VIEW wizeflow.tracks_view_by_smartlink_id_page_dt
+ENGINE = AggregatingMergeTree() 
+PARTITION BY left(ifNull(smartlink_id,'00'),2) 
+ORDER BY (smartlink_id, date, ifNull(page,0), visitor)
+POPULATE
+AS 
+select ifNull(smartlink_id,'0000') smartlink_id,
+toDate(dt) date,
+countIfState(event='open') page_views,
+sumIfState(toInt32OrZero(data), event='view') page_view_seconds,
+page,
+ifNull(visitor,'') visitor
+from wizeflow.tracks
+where fp!='' and visitor!='anonymous' and object='page' and event in ('view','open')
+group by smartlink_id, toDate(dt), page, visitor;
